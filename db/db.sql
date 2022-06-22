3087e797ac3240653a9965a6a0ef6ecb910cdd5b
Natali-Skv <nat-s.skv@mail.ru>
2022-06-22 04:05:57 +0300

    add alter system

--- db/db.sql
+++ db/db.sql
@@ -8,6 +8,20 @@ DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS votes CASCADE;
 DEALLOCATE ALL;
 
+CREATE EXTENSION IF NOT EXISTS citext;
+
+ALTER SYSTEM SET
+    checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET
+    wal_buffers = '6912kB';
+ALTER SYSTEM SET
+    default_statistics_target = '100';
+ALTER SYSTEM SET
+    random_page_cost = '1.1';
+ALTER SYSTEM SET
+    effective_io_concurrency = '200';
+
+
 CREATE UNLOGGED TABLE users
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
